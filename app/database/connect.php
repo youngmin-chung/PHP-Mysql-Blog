16eafe6b5faa567e3d29662a549e743e8a34dea5
Youngmin Chung <y_chung2@fanshaweonline.ca>
2021-02-16 23:39:34 -0500

    change db settings

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,11 +1,23 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'blog';
+// $host = 'localhost';
+// $user = 'root';
+// $pass = '';
+// $db_name = 'blog';
 
-$conn = new MySQLi($host, $user, $pass, $db_name);
+// $conn = new MySQLi($host, $user, $pass, $db_name);
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if ($conn->connect_error) {
     die('Database connection error: ' . $conn->connect_error);
