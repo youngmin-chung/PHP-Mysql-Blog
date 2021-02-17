2da412cac2792e93c0c55f187934ce52d7eb8640
Youngmin Chung <y_chung2@fanshaweonline.ca>
2021-02-16 23:51:33 -0500

    change minors

--- path.php
+++ path.php
@@ -1,5 +1,5 @@
 <?php 
 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
-define("BASE_URL", "https://php-mysql-myblog.herokuapp.com/"); 
+define("BASE_URL", "https://php-mysql-myblog.herokuapp.com"); 
 
