7cea639ff620969ec4dfa72c4892ee3d1100abe4
Youngmin Chung <mintmin01@gmail.com>
2021-03-23 12:00:43 -0400

    minor change

--- path.php
+++ path.php
@@ -2,5 +2,4 @@
 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
 define("BASE_URL", "https://php-mysql-myblog.herokuapp.com"); 
-define("UPLOAD_URL", "https://php-blog-bucket.s3.us-east-2.amazonaws.com")
-
+define("UPLOAD_URL", "https://php-blog-bucket.s3.us-east-2.amazonaws.com"); 
